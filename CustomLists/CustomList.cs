52f91e6b3bec92748926ca47d6fea171b1cea69d
joshweber18 <josh18weber@gmail.com>
2018-11-08 09:18:10 -0600

    Iforgot to commit at all

---
+++ CustomLists/CustomList.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomLists
+{
+    public class CustomList<T>
+    {
+
+        T[] originalArray;
+        int Count;
+
+        public CustomList()
+        {
+            Count = 0;
+            originalArray = new T[Count];
+        }
+
+        public void Add(T itemToAdd)
+        {
+            if (Count == 0)
+            {
+                Count++;
+                T[] tempArray = new T[Count];
+                tempArray[Count - 1] = itemToAdd;
+                originalArray = tempArray;
+            }
+            else
+            {
+                Count++;
+                T[] tempArray = new T[Count];
+                for (int i = 0; i < Count - 1; i++)
+                {
+                    tempArray[i] = originalArray[i];
+                }
+                tempArray[Count - 1] = itemToAdd;
+                originalArray = tempArray;
+            }
+        }
+
+        public void Remove(T itemToCheck)
+        {
+            bool contains = false;
+            for (int i = 0; i < Count; i++)
+            {
+                if (originalArray[i].Equals(itemToCheck))
+                {
+                    contains = true;
+                    break;
+                }
+            }
+
+            if (contains)
+            {
+                int j = 0;
+                T[] tempArray = new T[Count - 1];
+                for (int i = 0; i < Count; i++, j++)
+                {
+                    if (!itemToCheck.Equals(originalArray[i]))
+                    {
+                        tempArray[j] = originalArray[i];
+                    }
+                    else
+                    {
+                        j--;
+                    }
+                }
+                Count = Count - 1;
+                originalArray = new T[Count];
+                originalArray = tempArray;
+            }
+           
+        }
+    }
+}
--- CustomListsUnitTesting/UnitTest1.cs
+++ CustomListsUnitTesting/UnitTest1.cs
@@ -8,8 +8,32 @@ namespace CustomListsUnitTesting
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Add1Test()
         {
+            // Arrange
+            CustomList<int> list = new CustomList<int>();
+            int expected = 1;
+
+            // Act
+            list.add(4);
+
+            // Assert
+            Assert.AreEqual(expected, list.count);
+        }
+
+        [TestMethod]
+        public void Add2Test()
+        {
+            // Arrange
+            CustomList<int> list = new CustomList<int>();
+            int expected = 2;
+
+            // Act
+            list.add(4);
+            list.add(5);
+
+            // Assert
+            Assert.AreEqual()
         }
     }
 }
